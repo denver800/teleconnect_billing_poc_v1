6c0f56835c639ed579bfd2a8aeb2899ea9581f9f
MaB2210 <connect.mannbhatt@gmail.com>
2025-10-29 17:44:39 -0400

    run command

--- app/copy.py
+++ app/copy.py
@@ -1,1 +1,1 @@
- python -c "import sys; sys.path.insert(0, r'src\\proto_generated'); import apis.billdata.v2025_2_0.billdata_root_pb2 as m; print('OK', hasattr(m,'BillData'))"
+python -c "import sys; sys.path.insert(0, r'src\proto_generated'); import apis.billdata.v2025_2_0.billdata_root_pb2 as m; print('OK', hasattr(m,'BillData'))"
