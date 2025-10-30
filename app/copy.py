25444982fd50e2c86de690e00d8b36650cab254d
MaB2210 <connect.mannbhatt@gmail.com>
2025-10-30 09:41:32 -0400

    run command

--- app/copy.py
+++ app/copy.py
@@ -7,3 +7,13 @@ for d,_,_ in os.walk(base):
         p.write_text("")  # make it a package
 print("Init files created under", base)
 PY
+
+
+rmdir /s /q src\proto_generated
+mkdir src\proto_generated
+
+python -m grpc_tools.protoc ^
+ -I=src/proto ^
+ --python_out=src/proto_generated ^
+ --grpc_python_out=src/proto_generated ^
+ src/proto/apis/billdata/v2025_2_0/billdata_root.proto
